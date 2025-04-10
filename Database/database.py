cff95db44460edcc54e9a962956a12c5ef87ffaf
tkipke <tkipke@umich.edu>
2025-04-10 11:23:30 -0400

    setting up database, etc.

--- Database/database.py
+++ Database/database.py
@@ -41,12 +41,60 @@ def add_harvard_directors_from_json(conn, cur, filename):
 
 
 
+def create_met_directors_table(conn, cur):
+    cur.execute('''CREATE TABLE IF NOT EXISTS MetDirectors
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        start_year INTEGER,
+        end_year INTEGER)'''
+        )
+    conn.commit()
+
+def add_met_directors_from_json(conn, cur, filename):
+
+    with open(filename, 'r') as file:
+        content = json.load(file)
+
+    for item in content:
+        cur.execute('''INSERT OR IGNORE INTO MetDirectors (name,start_year,end_year) VALUES (?,?,?)''', 
+        (item['name'], item['start_year'], item['end_year']))
+
+    conn.commit()
+
+def create_met_art_table(conn, cur):
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS MetArt
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        objectID INTEGER UNIQUE,
+        artistGender INTEGER,
+        accessionYear INTEGER)''')
+    conn.commit()
+
+def add_met_art_from_json(conn, cur, filename):
+
+    with open(filename, 'r') as file:
+        content = json.load(file)
+
+    for item in content:
+        gender = cur.execute(f'''SELECT id FROM Gender 
+        WHERE {item['artistGender']} = Gender''')
+        # genders = cur.execute('''SELECT * FROM Gender''')
+        # if item['artistGender'] == 'Female':
+
+        cur.execute('''INSERT OR IGNORE INTO MetArt (objectID,artistGender,accessionYear) VALUES (?,?,?)''',
+        (item['objectID'], gender, item['accessionYear']))
+
+    conn.commit()
+
 def main():
     conn, cur = set_up_database("Art.db")
     create_gender_table(conn, cur)
     create_harvard_directors_table(conn, cur)
     add_harvard_directors_from_json(conn, cur, "harvard_directors.json")
-
+    create_met_directors_table(conn, cur)
+    add_met_directors_from_json(conn, cur, "met_directors.json")
+    create_met_art_table(conn, cur)
+    add_met_art_from_json(conn, cur, "met.json")
 
 if __name__ == "__main__":
     main()
